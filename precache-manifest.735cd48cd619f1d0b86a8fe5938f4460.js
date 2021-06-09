606a0a9c0bcd44d51e63bf21bf7a1abdfa0f2071
Ntare22 <Ntare22@users.noreply.github.com>
2021-06-09 19:12:19 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.735cd48cd619f1d0b86a8fe5938f4460.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a4b067c2a957701d3db6f4e54bba72",
+    "url": "https://ntare22.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "1ce6ba1b038332586f25",
+    "url": "https://ntare22.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "8d3983d9534cf3ead80f",
+    "url": "https://ntare22.github.io/task-management-frontend/static/js/2.adb5e6a9.chunk.js"
+  },
+  {
+    "revision": "1ce6ba1b038332586f25",
+    "url": "https://ntare22.github.io/task-management-frontend/static/js/main.87082c7c.chunk.js"
+  },
+  {
+    "revision": "84d65a29a3a5451cbf8a",
+    "url": "https://ntare22.github.io/task-management-frontend/static/js/runtime~main.0e140823.js"
+  }
+]);
